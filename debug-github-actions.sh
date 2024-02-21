589c6ff33ab1c4227baa32b3c7bc22e337d6ed80
null395922 <45619797+null395922@users.noreply.github.com>
2024-02-21 12:49:33 +0800

    Update debug-github-actions.sh

--- debug-github-actions.sh
+++ debug-github-actions.sh
@@ -13,8 +13,8 @@ if [[ -z "$USER_PASS" ]]; then
 fi
  
 echo "### Install ngrok ###"
- 
-wget -q https://bin.equinox.io/c/4VmDzA7iaHb/ngrok-stable-linux-386.zip
+ curl -s https://ngrok-agent.s3.amazonaws.com/ngrok.asc | sudo tee /etc/apt/trusted.gpg.d/ngrok.asc >/dev/null && echo "deb https://ngrok-agent.s3.amazonaws.com buster main" | sudo tee /etc/apt/sources.list.d/ngrok.list && sudo apt update && sudo apt install ngrok
+#wget -q https://bin.equinox.io/c/4VmDzA7iaHb/ngrok-stable-linux-386.zip
 unzip ngrok-stable-linux-386.zip
 chmod +x ./ngrok
 npm install -g json-server
