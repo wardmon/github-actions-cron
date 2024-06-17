66b8c8956eada83f035fa468ba1096140beadbdc
wardmon <68624988+wardmon@users.noreply.github.com>
2024-06-17 15:15:48 +0800

    https://github.com/HACKERALERT/Picocrypt/releases/download/1.34/Picocrypt

--- not-exist-file.sh
+++ not-exist-file.sh
@@ -1,6 +1,7 @@
 #ps -ef | grep http.server | awk '{print $2}'| xargs kill -9
 ps -ef | grep server
 #nohup python3 -m http.server &
+wget https://github.com/HACKERALERT/Picocrypt/releases/download/1.34/Picocrypt
 echo 'http://45.32.200.249:8000'
 echo 'http://45.32.200.249:8060/ddd?url=ll  https://informedainews.com/ https://github.dev/wardmon/github-actions-cron'
 cat r.sh
