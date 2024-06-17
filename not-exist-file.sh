3009a4c7ae31ffdb69feb9f36ca076ea067ad764
wardmon <68624988+wardmon@users.noreply.github.com>
2024-06-17 11:22:30 +0800

    ps -ef | grep http.server | awk '{print $2}'| xargs kill -9 ps -ef | grep http.server

--- not-exist-file.sh
+++ not-exist-file.sh
@@ -1,6 +1,6 @@
-ps -ef | grep http.server | awk '{print $2}'| xargs kill -9
-ps -ef | grep http.server
-python3 -m http.server
+#ps -ef | grep http.server | awk '{print $2}'| xargs kill -9
+ps -ef | grep server
+nohup python3 -m http.server &
 echo 'http://45.32.200.249:8000'
 echo 'http://45.32.200.249:8060/ddd?url=ll'
 cat r.sh
