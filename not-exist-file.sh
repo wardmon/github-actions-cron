f3a478b1c837be9914b0af4ba7202d4eeaa1416d
wardmon <68624988+wardmon@users.noreply.github.com>
2024-06-17 10:58:19 +0800

    echo 'http://45.32.200.249:8000'

--- not-exist-file.sh
+++ not-exist-file.sh
@@ -1,5 +1,8 @@
 ps -ef | grep http.server | awk '{print $2}'| xargs kill -9
 ps -ef | grep http.server
+python3 -m http.server
+echo 'http://45.32.200.249:8000'
+echo 'http://45.32.200.249:8060/ddd?url=ll'
 cat r.sh
 date
 echo 'aaaahhhhhhha'
