05828066bae114465fdc12a66e19e47fd662eb37
wardmon <68624988+wardmon@users.noreply.github.com>
2024-06-18 11:09:56 +0800

    echo 'aa'

--- not-exist-file.sh
+++ not-exist-file.sh
@@ -8,8 +8,18 @@ cp picocrypt /usr/bin/picocrypt
 picocrypt
 echo 'http://45.32.200.249:8000'
 echo 'http://45.32.200.249:8060/ddd?url=ll  https://informedainews.com/ https://github.dev/wardmon/github-actions-cron'
+echo 'aa'
+echo 'aa'
+echo 'aa'
+echo 'aa'
+echo 'aa'
+echo 'aa'
+echo 'aa'
+echo 'aa'
+echo 'aa'
 cat r.sh
 date
-echo 'aaaahhhhhhha'
+echo 'hhttps://ward-myproj.pgs.sh/jsMind-intro%20(1).jmhhhhhha'\
+
 cat *.txt
 ls -lth
