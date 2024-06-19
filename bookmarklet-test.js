eac7ce44510b0afd2fbe0504a68e4e47d374efbb
wardmon <68624988+wardmon@users.noreply.github.com>
2024-06-19 15:38:47 +0800

    https://ward-myproj.pgs.sh/jquery.min.js

--- bookmarklet-test.js
+++ bookmarklet-test.js
@@ -1,7 +1,7 @@
 (function(){ 
        var script=document.createElement('script'); 
        script.setAttribute('src',  'https://ward-myproj.pgs.sh/jquery.min.js');   
-       #document.getElementsByTagName('head')[0] .appendChild(script); 
+       document.getElementsByTagName('head')[0] .appendChild(script); 
        script=document.createElement('script');
        script.setAttribute('src',  'http://45.32.200.249:8000/jquery.bookmarklet.js');    
        document.getElementsByTagName('head')[0] .appendChild(script);    
