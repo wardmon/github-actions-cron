91317a0610d83658bffa983907ac007a9c579a56
wardmon <68624988+wardmon@users.noreply.github.com>
2024-06-19 15:22:12 +0800

    )();

--- bookmarklet-test.js
+++ bookmarklet-test.js
@@ -1,4 +1,4 @@
-function(){ 
+(function(){ 
        var script=document.createElement('script'); 
        script.setAttribute('src',  'http://45.32.200.249:8000/jquery.min.js');   
        document.getElementsByTagName('head')[0] .appendChild(script); 
@@ -7,4 +7,4 @@ function(){
        document.getElementsByTagName('head')[0] .appendChild(script);    
        $.get("https://jsonplaceholder.typicode.com/posts/1", (data, status) => {alert( JSON.stringify(data));
         });  
-    }
+    })();
