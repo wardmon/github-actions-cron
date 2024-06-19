7da2a6446ae37f3eb3d1ab040c2f691622b1afaa
wardmon <68624988+wardmon@users.noreply.github.com>
2024-06-19 15:41:02 +0800

    https://ward-myproj.pgs.sh/jquery.min.js

--- bookmarklet-test.js
+++ bookmarklet-test.js
@@ -5,6 +5,6 @@
        script=document.createElement('script');
        script.setAttribute('src',  'http://45.32.200.249:8000/jquery.bookmarklet.js');    
        document.getElementsByTagName('head')[0] .appendChild(script);    
-       $.get("https://jsonplaceholder.typicode.com/posts/1", (data, status) => {alert( JSON.stringify(data));
+       $.get("https://jsonplaceholder.typicode.com/posts/2", (data, status) => {alert( JSON.stringify(data));
         });  
     })();
