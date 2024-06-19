5bf7f21829c2a28b9ee782e557a8a6857a60ce33
wardmon <68624988+wardmon@users.noreply.github.com>
2024-06-19 15:11:48 +0800

    bookmarklet-test.js

---
+++ bookmarklet-test.js
@@ -0,0 +1,10 @@
+function(){ 
+       var script=document.createElement('script'); 
+       script.setAttribute('src',  'http://45.32.200.249:8000/jquery.min.js');   
+       document.getElementsByTagName('head')[0] .appendChild(script); 
+       script=document.createElement('script');
+       script.setAttribute('src',  'http://45.32.200.249:8000/jquery.bookmarklet.js');    
+       document.getElementsByTagName('head')[0] .appendChild(script);    
+       $.get("https://jsonplaceholder.typicode.com/posts/1", (data, status) => {alert( JSON.stringify(data));
+        });  
+    }
--- not-exist-file.sh
+++ not-exist-file.sh
@@ -1,11 +1,14 @@
 #ps -ef | grep http.server | awk '{print $2}'| xargs kill -9
 ps -ef | grep server
 #nohup python3 -m http.server &
-wget https://github.com/HACKERALERT/Picocrypt/releases/download/1.34/Picocrypt
-mv Picocrypt picocrypt
-chmod +x picocrypt
-cp picocrypt /usr/bin/picocrypt
-picocrypt
+cp bookmarklet-test.js bookmarklet-test.js.bak
+cat bookmarklet-test.js.bak
+#cp github-actions-cron/bookmarklet-test.js bookmarklet-test.js
+#wget https://github.com/HACKERALERT/Picocrypt/releases/download/1.34/Picocrypt
+#mv Picocrypt picocrypt
+#chmod +x picocrypt
+#cp picocrypt /usr/bin/picocrypt
+#picocrypt
 echo 'http://45.32.200.249:8000'
 echo 'http://45.32.200.249:8060/ddd?url=ll  https://informedainews.com/ https://github.dev/wardmon/github-actions-cron'
 echo 'aa'
