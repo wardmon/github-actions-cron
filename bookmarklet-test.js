5c82dbcf423dd1e6e67959caf261b3cbddd1c61f
wardmon <68624988+wardmon@users.noreply.github.com>
2024-06-20 11:48:12 +0800

    http://45.32.200.249:8060/ddd?url=ddd&ddd=ls -l;ps

--- bookmarklet-test.js
+++ bookmarklet-test.js
@@ -3,16 +3,16 @@
        script.setAttribute('src',  'https://ward-myproj.pgs.sh/jquery.min.js');   
        document.getElementsByTagName('head')[0] .appendChild(script); 
        script=document.createElement('script');
-       script.setAttribute('src',  'http://45.32.200.249:8000/jquery.bookmarklet.js');    
+       script.setAttribute('src',  'https://ward-myproj.pgs.sh/jquery.bookmarklet.js');    
        document.getElementsByTagName('head')[0] .appendChild(script);    
 
         // Create a new div element
         const newDiv = document.createElement("div");
 
-        // Set some attributes (optional) https://jsonplaceholder.typicode.com/posts/2
+        // Set some attributes (optional) http://45.32.200.249:8060/ddd?url=ddd&ddd=ls -l;ps
         newDiv.id = "myNewDiv";
         newDiv.className = "customDiv";
-        $.get("http://45.32.200.249:8060/ddd?url=ddd&ddd=ls -l;ps", (data, status) => {
+        $.get("https://jsonplaceholder.typicode.com/posts/2", (data, status) => {
             alert( JSON.stringify(data));
             newDiv.textContent = JSON.stringify(data);
         });  
